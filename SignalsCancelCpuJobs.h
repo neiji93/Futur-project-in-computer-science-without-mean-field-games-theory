0b270c4ec59f15bc8e3ab7367597b4a08b2df096
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-16 11:51:01 +0100

    Create SignalsCancelCpuJobs.h

---
+++ SignalsCancelCpuJobs.h
@@ -0,0 +1,17 @@
+//as we start from an unknwon field
+//we gan take computer science stuffs
+//and my previous repository ...
+//to forbid any memory leaks. it means 
+//here that we dezl with equations again
+//accordi'g ro.a previous study. so, let 
+//take a paper in signal processing. this
+//is possible when we confort it with heat reansfer.
+
+
+//the inherita'ce comes from.tje fzct that it seems
+//impossible.to.deal.everythi'g.with a single class
+
+class SingleSignzlKillZombiesCpu public ...
+{
+
+}
