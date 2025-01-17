3eab86eb9582facaef0a906a23912af67caf41b1
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-17 11:44:24 +0100

    Create NonSpinnorsSoverRealTime.h

---
+++ NonSpinnorsSoverRealTime.h
@@ -0,0 +1,7 @@
+
+//1/  conservation des formes automorph et 
+//mode de dilension superieur a exp(11)
+//2/  wealth maximization 
+//3/ i forgot
+
+class forceSpinnorsIterators : public notquantizised
