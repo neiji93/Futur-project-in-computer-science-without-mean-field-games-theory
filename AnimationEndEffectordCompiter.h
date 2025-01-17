b33df452d98dcfe436be2d6e44b710318fc9b740
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-17 09:26:48 +0100

    Update AnimationEndEffectordCompiter.h

--- AnimationEndEffectordCompiter.h
+++ AnimationEndEffectordCompiter.h
@@ -15,3 +15,19 @@ class AnimationEndEffecorsComputer
 
    void...
 }
+
+
+//there must be a way to turn those properties
+//into physics. then, you can choose to ude your
+//game engine animation logics or your game
+//engine physic engine (sorry to be more clear i spoke in french)
+
+
+//....
+//we can still use most seen functions in high school.
+class physicalQuangizisDatas
+{
+
+public:
+  void exponentialInstantRadiosityChekck();
+}
