01ed69581a8cb9007da5229868fc807ebd5688ab
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-17 22:21:26 +0100

    Create ToolsForceMorphismHitchin.h

---
+++ ToolsForceMorphismHitchin.h
@@ -0,0 +1,30 @@
+
+//as wa can sometimes switch between physic engine
+//and animation engine, we have mutual exclusions.
+//then, only one single morphism is needed.
+//a tool here will be to do something like extrudi'g
+//medhes. and as we wont use genetic algorithms,
+//there must be an algorithm to delect (pdf).
+
+//we didnt inherited anything but to have lauerd.
+//it is ghe second time in my projects that i have laherd 
+//so we will be able to patch them. then the algorithm seems not iterative.
+class ToolExteude : public baseObject
+{}
+
+class nonIterativeHitchinGroupsAlgorithm
+{
+public:
+
+  void doNotIterateWorld()
+{
+  //grt all end effectors of every skelegond
+  //...
+
+
+   //use immediatemy compiter vision field to forbid separating diffuse testures like
+   //componenets to split. it does't need quantizatio . then, we can have a realtime behaviour 
+
+  
+}
+}
