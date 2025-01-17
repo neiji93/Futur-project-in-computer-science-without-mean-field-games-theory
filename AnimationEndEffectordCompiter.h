1fae6d44391e2c619348d9e60aa630878d079c46
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-17 07:51:33 +0100

    Create AnimationEndEffectordCompiter.h

---
+++ AnimationEndEffectordCompiter.h
@@ -0,0 +1,17 @@
+//maybe there are formal series associated
+//with some automorphism at 11 dimensions
+
+
+//there is no really inherita'ce to add. sorry here
+//this is not really commented
+
+class AnimationEndEffecorDatas
+{}
+
+class AnimationEndEffecorsComputer
+{
+ public:
+   void geneticAlgorithmsPreventThem();
+
+   void...
+}
